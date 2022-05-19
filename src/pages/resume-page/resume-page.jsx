3da43bc9f3fd05323b7cf7927e017cf034cd9b67
diorm47 <dior47m@gmail.com>
2022-05-19 16:15:48 +0500

    added link to CV

--- src/pages/resume-page/resume-page.jsx
+++ src/pages/resume-page/resume-page.jsx
@@ -5,11 +5,19 @@ function ResumePage() {
   return (
     <div className="resume ">
       <a
-        href="https://drive.google.com/file/d/1Z8sxpQV_I5ygbVtHtBxQL_dItAHOAYRC/view?usp=sharing"
+        href="https://drive.google.com/file/d/1OGkDkhrVUmvWtQ1Io6Jky5_JYYLYRMtV/view?usp=sharing"
         target="_blank"
         rel="noreferrer"
       >
-        <button className="resume__button">View</button>
+        <button className="resume__button">View (ENG)</button>
+      </a>
+
+      <a
+        href="https://drive.google.com/file/d/1h_WlwRO0VpSGuL-Xz_YToINC70KMk6mw/view?usp=sharing"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button className="resume__button eng">View (RU)</button>
       </a>
     </div>
   );
